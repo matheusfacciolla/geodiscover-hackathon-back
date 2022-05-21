efe1d78f7e3a8423a2e68ae927a168e09d086379
matheusfacciolla <matheustassifacciolla@gmail.com>
2022-05-20 22:42:30 -0300

    fix: fix question controller

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -2,26 +2,22 @@ import db from "../db.js";
 import { ObjectId } from "mongodb";
 
 async function questions(req, res) {
-    const { type, id } = req.query;
+    const { type } = req.query;
 
     try {
         if (type === "random") {
             const allQuestions = await db.collection("questions").find({}).toArray();
             const allQuestionsRandom = allQuestions.sort(comparator);
 
-            const findId = await db.collection("questions").findOne({ _id: new ObjectId(id) });
-
-            if (findId._id != allQuestionsRandom[0]._id) {
-                res.status(200).send({
-                    image: allQuestionsRandom[0].image, id: allQuestionsRandom[0]._id,
-                    alternatives: [
-                        { name: allQuestionsRandom[0].name, type: allQuestionsRandom[0].type, isCorrect: true },
-                        { name: allQuestionsRandom[1].name, type: allQuestionsRandom[1].type, isCorrect: false },
-                        { name: allQuestionsRandom[2].name, type: allQuestionsRandom[2].type, isCorrect: false },
-                        { name: allQuestionsRandom[3].name, type: allQuestionsRandom[3].type, isCorrect: false }
-                    ]
-                });
-            }
+            res.status(200).send({
+                image: allQuestionsRandom[0].image, id: allQuestionsRandom[0]._id,
+                alternatives: [
+                    { name: allQuestionsRandom[0].name, type: allQuestionsRandom[0].type, isCorrect: true },
+                    { name: allQuestionsRandom[1].name, type: allQuestionsRandom[1].type, isCorrect: false },
+                    { name: allQuestionsRandom[2].name, type: allQuestionsRandom[2].type, isCorrect: false },
+                    { name: allQuestionsRandom[3].name, type: allQuestionsRandom[3].type, isCorrect: false }
+                ]
+            });
         }
 
         if (type === "game") {
